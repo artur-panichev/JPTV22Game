19f376aebe1605abb16697cafc5e33bdee1da476
pupil <pupil@ivkhk.ee>
2023-09-28 11:06:23 +0300

    added tries and help

--- src/jptv22game/App.java
+++ src/jptv22game/App.java
@@ -14,30 +14,60 @@ import java.util.Scanner;
  */
 public class App {
 
-    Random random = new Random();
+    final Random random;
+    final Scanner scanner;
+    boolean repeat;
     
     public App() {
         this.random = new Random();
+         this.scanner = new Scanner(System.in);
+         this.repeat = true;
     }
     
     
     public void run(){
+        System.out.println("----- Game -----");
+        do{
+            boolean win = false;
+            int tries = 3;
+            int number = this.random.nextInt(9)+1;
+            System.out.println("Угадайте число от 1 до 9");
+            do{
+                int userInt;
+                do{
+                    try{
+                        System.out.print("Ваш ответ: ");
+                        userInt = this.scanner.nextInt(); this.scanner.nextLine();
+                        break;
+                    } catch(Exception e){
+                        this.scanner.nextLine();
+                        System.out.println("Не верный ввод!");
+                    } 
+                } while(true);
+
+                if(userInt == number){
+                    System.out.println("Вы победили");
+                    win = true;
+                } else if(userInt > number){
+                    System.out.println("Загаданное число меньше");
+                } else{
+                    System.out.println("Загаданное число больше");
+                }
+                tries--;
+            }while(tries > 0 && win == false);
+            if(!win){
+                System.out.println("Вы проиграли! Задуманное число: " + number);
+
+            }
+            System.out.print("Для выхода введите \"q\" или любой другой символ для продолжения: ");
+            String userExit = this.scanner.nextLine();
+
+            if("q".equals(userExit)){
+                this.repeat = false;
+            }
+        }while(this.repeat);
         
-        int number = this.random.nextInt(10)+1;
-        
-        Scanner scanner = new Scanner(System.in);
-        
-        System.out.println("Угадайте число от 1 до 10");
-        
-        System.out.print("Ваш ответ: ");
-        int answer = scanner.nextInt(); scanner.nextLine();
-        
-        if(answer == number){
-            System.out.println("Вы победили");
-        }
-        else{
-            System.out.println("Вы проиграли!");
-        }
+        System.out.println("----- End Game -----");
 
     }
 }
